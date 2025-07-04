772ace4f69d58141973cf58794bb1ee14d0b657e
Heymun Pareek <78228395+PassionateCoder340@users.noreply.github.com>
2025-07-04 12:26:54 +0530

    Added dummy files for compilation to work

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log(err));
+    
 process.on('SIGINT', async ()=> {
     await mongoose.connection.close();
     console.log("App termination: MongoDB disconnected");
